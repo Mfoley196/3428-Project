8bf5ebdcbbcb9c472df2388a3479b36bfcf9b810
Kojo Acquah <kojo.acquah@hotmail.com>
2017-03-25 14:43:22 -0300

    bug fixes for testing

--- js/scripts.js
+++ js/scripts.js
@@ -2,57 +2,53 @@
 
 $(document).ready(function () { //wait for all documents to fully load
 
-    prep();
+    try {
+        prep();
+        startTimer();
 
-    //checks if the location tracking has been enabled for the page in the browser
-    navigator.permissions.query({'name':'geolocation'}).then(function (result) {
+        //checks if the location tracking has been enabled for the page in the browser
+        navigator.permissions.query({'name': 'geolocation'}).then(function (result) {
+            console.log("This is my geolocation permission state: " + result.state);
 
-        // console.log("This is my geolocation permission state: " + result.state);
+            if (result.state === "denied") {
+                var err = new Error("Location could not be generated because you have it blocked. Please allow location" +
+                    "tracking in your browser to use the program");
+                err.name = "Location Access Denied";
+                //if the request has been denied then throw error and notify user then close after 5s
+                errorHandler(err)
+            }
+        });
 
-        if (result.state === 'denied') {
-            var error = new Error("Location could not be generated because you have it blocked. Please allow location" +
-                "tracking in your browser to use the program");
-            error.name = "Location Access Denied";
-            // if the request has been denied then throw error and notify user
-            errorHandler(error);
-        }
-    });
-        // else if (result.state === 'prompt') {
-        //     var error = new Error("Please grant permissions for location tracking in your browser");
-        //     error.name = "Request Location Access";
-        //     //if the permission is still in "prompt" status, throw error and restart after 30 seconds
-        //     errorHandler(error);
-        //     setTimeout(function () {
-        //         location.reload();
-        //     }, TIME_INTERVAL * 1000); //reload after 30 seconds
+        // if location tracking privileges are granted, run the main program
+        try {
 
-        // } else if (result.state === 'granted') {
+            setTimeout(function () {
 
-            prep();
-            // if location tracking privileges are granted, run the main program
-            try {
-                testDevice();
-                startTimer();
-                setTimeout(function () {
-                    main();
-                }, 10000);
+                main();
+
                 setInterval(function () {
                     main()
-                }, TIME_INTERVAL * 1000);
-            } catch (e) {
-                errorHandler(e);
-            }
-        // }
-    // });
+                }, REFRESH_RATE * 1000);
+
+            }, 10000);
+
+        } catch (e) {
+            errorHandler(e);
+        }
+
+    } catch (e) {
+        errorHandler(e);
+        console.log(e.toString());
+    }
 
 
     /*UI event handlers*/
-    //reload when the button is clicked
+//reload when the button is clicked
     $('#reloadApp').click(function () {
         location.reload(true);
     });
 
-    //exit the app when the button is click
+//exit the app when the button is click
     $('#exitApp').click(function () {
         window.close();
     });
@@ -61,10 +57,10 @@ $(document).ready(function () { //wait for all documents to fully load
         window.close();
     });
 
-    //open user guide modal on click
+//open user guide modal on click
     $('.help').click(function () {
         $('#myHelp').modal('show');
     });
-
-});
+})
+;
 
