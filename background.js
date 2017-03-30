8b4902fabec24d2b7b8ec129d7d939d87292092b
ShanilKoshitha <shanilkoshitha@rocketmail.com>
2017-03-30 20:03:06 -0300

    Extension fix
    Replacing the current background.js and content.js files with another
    version

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
-<<<<<<< Updated upstream
+
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.create({"url": "http://spotify.com"});
+    chrome.tabs.create({"url": "http://locations.github.io"});
   // Send a message to the active tab
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     var activeTab = tabs[0];
@@ -16,22 +16,4 @@ chrome.runtime.onMessage.addListener(
   }
 );
 
-=======
-// Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.create({"url": "http://spotify.com"});
-  // Send a message to the active tab
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
-  });
-});
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if( request.message === "open_new_tab" ) {
-      chrome.tabs.create({"url": request.url});
-    }
-  }
-);
 
->>>>>>> Stashed changes
